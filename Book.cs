f5db2a4bec1bcfd7915390057dad4de8fd5fa92c
jmmalcom <jmmalcom@crimson.ua.edu>
2021-11-22 13:14:30 -0600

    Rough

---
+++ Book.cs
@@ -0,0 +1,51 @@
+namespace mis221_Composition_Lab
+{
+    public class Book
+    {
+        private string isbn;
+        private string title;
+        private Author author;
+
+        public Book(string tempIsbn, string tempTitle, Author tempAuthor)
+        {
+            isbn = tempIsbn;
+            title = tempTitle;
+            author = tempAuthor;
+        }
+
+        public string GetIsbn()
+        {
+            return isbn;
+        }
+
+        public void SetIsbn(string tempIsbn)
+        {
+            isbn = tempIsbn;
+        }
+
+        public string GetTitle()
+        {
+            return title;
+        }
+
+        public void SetTitle(string tempTitle)
+        {
+            title = tempTitle;
+        }
+
+        public Author GetAuthor()
+        {
+            return author;
+        }
+
+        public void SetAuthor(Author tempAuthor)
+        {
+            author = tempAuthor;
+        }
+
+        public override string ToString()
+        {
+            return "isbn = " + isbn + " title = " + title + author.ToString();
+        }
+    }
+}
